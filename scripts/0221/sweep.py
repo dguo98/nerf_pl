ad68fedf432bc76de428aeaccb97f24bb0f57c0b
Demi Guo <demiguo98@gmail.com>
2022-02-21 13:10:01 -0800

    add trans sigma reg, large initial mlp

---
+++ scripts/0221/sweep.py
@@ -0,0 +1,125 @@
+import numpy as np
+import os
+import sys
+
+exp_id = 94
+
+def generate(gpu, epoch, bs, lr, img_ds, model, box_warp, n_feat, back_res, use_xyz_net, n_xyz_dim,mode,temp, lambda_u):
+    global exp_id
+    exp_id += 1
+    exp_name = "T%05d" % exp_id
+    print("exp_name=",exp_name)
+
+    f = open(temp)
+    lines = f.readlines()
+    f.close()
+
+    lines[0] = f"EXP_NAME={exp_name}\n"
+    lines[8] = f"GPU={gpu}\n"
+    lines[9] = f"BOX_WARP={box_warp:.2f}\n"
+    lines[10] = f"IMG_DS={img_ds}\n"
+    lines[11] = f"MODEL={model}\n"
+    lines[12] = f"N_FEAT={n_feat}\n"
+    lines[13] = f"BACKBONE_RES={back_res}\n"
+    lines[14] = f"BS={bs}\n"
+    lines[15] = f"LR={lr:.7f}\n"
+    lines[16] = f"EPOCHS={epoch}\n"
+    lines[17] = f"USE_XYZ_NET={use_xyz_net}\n"
+    lines[18] = f"N_XYZ_DIM={n_xyz_dim}\n"
+    lines[19] = f"MODE={mode}\n"
+    lines[20]= f"LAMBDA_U={lambda_u}\n"
+
+    f = open("%s.sh" % exp_name, "w")
+    for line in lines:
+        f.write(line)
+    f.close()
+    os.system("chmod +x %s.sh" % exp_name)
+    return "%s.sh" % exp_name
+    
+if __name__ == "__main__":
+
+    epoch=20  # sufficient for benchmark
+    bs=1024
+    lr=5e-4
+    img_ds=8
+    model="NeRFTriplane"
+    box_warp=1.0
+    n_feat=48
+    back_res=512
+    use_xyz_net=1
+    n_xyz_dim=128
+    mode="debug"
+    lambda_u=0.01
+    
+    # use gpu=1,2,3,4,5,6
+    
+    ind = 0
+    scripts = {}
+     
+    for temp in ["template.sh"]:
+        for lr in [5e-4]:
+            for box_warp in [2.0]:
+                for model in ["NeRF"]:
+                    if model == "NeRFTriplane":
+                        back_res=512
+                        mode="debug5"
+                    else:
+                        back_res=128
+                        mode="default"
+
+
+                    gpu=(ind%5)
+                    ind+=1
+                    script = generate(gpu, epoch, bs, lr, img_ds, model, box_warp, n_feat, back_res, use_xyz_net, n_xyz_dim, mode, temp,lambda_u)
+                    if not gpu in scripts:
+                        scripts[gpu] = []
+                    scripts[gpu].append(script)
+
+    for temp in ["template.sh"]:
+        for lr in [5e-4]:
+            for box_warp in [2.0]:
+                for model in ["NeRFTriplane"]:
+                    for mode in ["debug5", "debug6"]:
+                        if model == "NeRFTriplane":
+                            back_res=512
+                            mode="debug5"
+                        else:
+                            back_res=128
+                            mode="default"
+
+
+                        gpu=(ind%5)
+                        ind+=1
+                        script = generate(gpu, epoch, bs, lr, img_ds, model, box_warp, n_feat, back_res, use_xyz_net, n_xyz_dim, mode, temp,lambda_u)
+                        if not gpu in scripts:
+                            scripts[gpu] = []
+                        scripts[gpu].append(script)
+        
+    for temp in ["template.sh"]:
+        for lr in [5e-4]:
+            for box_warp in [2.0]:
+                for model in ["NeRFTriplane"]:
+                    for mode in ["debug5"]:
+                        for lambda_u in [0.1, 0.05]:
+                            if model == "NeRFTriplane":
+                                back_res=512
+                                mode="debug5"
+                            else:
+                                back_res=128
+                                mode="default"
+
+
+                            gpu=(ind%5)
+                            ind+=1
+                            script = generate(gpu, epoch, bs, lr, img_ds, model, box_warp, n_feat, back_res, use_xyz_net, n_xyz_dim, mode, temp, lambda_u)
+                            if not gpu in scripts:
+                                scripts[gpu] = []
+                            scripts[gpu].append(script)
+       
+
+    for gpu in scripts:
+        f = open(f"sweep_gpu{gpu}.sh", "w")
+        cmd = ";".join(["./" + s for s in scripts[gpu]] )
+        f.write(cmd + "\n")
+        f.close()
+        os.system(f"chmod +x sweep_gpu{gpu}.sh")
